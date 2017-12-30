f2de2e4c8d307dcb01ddafb9245c4f5e3abeceb1
munstein <eduardo.munstein@live.com>
2017-12-29 22:29:51 -0200

    Just refactoring again and again and again.

--- core/src/com/munstein/flappywave/FlappyWave.java
+++ core/src/com/munstein/flappywave/FlappyWave.java
@@ -18,231 +18,231 @@ import java.util.Random;
 
 public class FlappyWave extends ApplicationAdapter {
 
-	SpriteBatch batch;
-	Texture background;
-	Texture gameOverBackground;
-	Texture topTube;
-	Texture bottomTube;
-	Texture[] birds;
+    SpriteBatch batch;
+    Texture background;
+    Texture gameOverBackground;
+    Texture topTube;
+    Texture bottomTube;
+    Texture[] birds;
 
-	int flapState = 0;
-	int numberOfTubes = 4;
-	int score;
-	int scoringTube = 0;
-	int gameState = 0; //0 = Game is waiting player interaction / 1 = Game is running / 2 = Game over screen
-	final int fontScale = 8;
+    int flapState = 0;
+    int numberOfTubes = 4;
+    int score;
+    int scoringTube = 0;
+    int gameState = 0; //0 = Game is waiting player interaction / 1 = Game is running / 2 = Game over screen
+    final int fontScale = 8;
 
-	long endtime = 0;
+    long endtime = 0;
 
-	float gravity = 2;
-	float recordedHeight;
-	float gap = 400;
-	float maxTubeOffset;
-	float birdY = 0;
-	float velocity = 0;
-	float tubeVelocity = 4;
-	float distanceBetweenTubes;
-	float endgameTextWidth, startGameTextWidth, highScoreTextWidth;
-	float[] tubeX = new float[numberOfTubes];
-	float[] tubeOffset = new float[numberOfTubes];
+    float gravity = 2;
+    float recordedHeight;
+    float gap = 400;
+    float maxTubeOffset;
+    float birdY = 0;
+    float velocity = 0;
+    float tubeVelocity = 4;
+    float distanceBetweenTubes;
+    float endgameTextWidth, startGameTextWidth, highScoreTextWidth;
+    float[] tubeX = new float[numberOfTubes];
+    float[] tubeOffset = new float[numberOfTubes];
 
-	Random randomGenerator;
+    Random randomGenerator;
 
-	Circle birdCircle;
+    Circle birdCircle;
 
-	Rectangle[] topTubeRectangles;
-	Rectangle[] bottomTubeRectangles;
+    Rectangle[] topTubeRectangles;
+    Rectangle[] bottomTubeRectangles;
 
-	BitmapFont scoreMessage;
-	BitmapFont highScoreMessage;
-	BitmapFont endGameMessage;
-	BitmapFont startGameMessage;
-	BitmapFont gameOverMessage;
+    BitmapFont scoreMessage;
+    BitmapFont highScoreMessage;
+    BitmapFont endGameMessage;
+    BitmapFont startGameMessage;
+    BitmapFont gameOverMessage;
 
-	Music sound;
+    Music sound;
 
-	Preferences preferences;
-	final String PREFERENCES_NAME = "preferences";
-	final String PREFERENCES_HIGHSCORE = "highscore";
+    Preferences preferences;
 
-	final String END_GAME_MESSAGE = "R E S T A R T";
-	final String START_GAME_MESSAGE = "F I G H T !";
-	final String HIGH_SCORE_MESSAGE = "R E C O R D : ";
+    final String PREFERENCES_HIGHSCORE = "highscore";
 
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		background = new Texture("bg.jpg");
-		gameOverBackground = new Texture("gameover.png");
+    final String END_GAME_MESSAGE = "R E S T A R T";
+    final String START_GAME_MESSAGE = "F I G H T !";
+    final String HIGH_SCORE_MESSAGE = "R E C O R D : ";
 
-		birdCircle = new Circle();
-		scoreMessage = new BitmapFont();
-		scoreMessage.setColor(Color.WHITE);
-		scoreMessage.getData().setScale(fontScale);
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        background = new Texture("bg.jpg");
+        gameOverBackground = new Texture("gameover.png");
 
-		endGameMessage = new BitmapFont();
-		endGameMessage.setColor(Color.PINK);
-		endGameMessage.getData().setScale(fontScale);
+        birdCircle = new Circle();
+        scoreMessage = new BitmapFont();
+        scoreMessage.setColor(Color.WHITE);
+        scoreMessage.getData().setScale(fontScale);
 
-		startGameMessage = new BitmapFont();
-		startGameMessage.setColor(Color.PURPLE);
-		startGameMessage.getData().setScale(fontScale);
+        endGameMessage = new BitmapFont();
+        endGameMessage.setColor(Color.PINK);
+        endGameMessage.getData().setScale(fontScale);
 
-		gameOverMessage = new BitmapFont();
-		gameOverMessage.setColor(Color.WHITE);
-		gameOverMessage.getData().setScale(fontScale);
+        startGameMessage = new BitmapFont();
+        startGameMessage.setColor(Color.PURPLE);
+        startGameMessage.getData().setScale(fontScale);
 
-		highScoreMessage = new BitmapFont();
-		highScoreMessage.setColor(Color.CYAN);
-		highScoreMessage.getData().setScale(fontScale);
+        gameOverMessage = new BitmapFont();
+        gameOverMessage.setColor(Color.WHITE);
+        gameOverMessage.getData().setScale(fontScale);
 
-		preferences = Gdx.app.getPreferences("preferences");
-		birds = new Texture[3];
+        highScoreMessage = new BitmapFont();
+        highScoreMessage.setColor(Color.CYAN);
+        highScoreMessage.getData().setScale(fontScale);
 
-		birds[0] = new Texture("playerup.png"); //face is up
-		birds[1] = new Texture("playerfall.png"); // face is down
+        preferences = Gdx.app.getPreferences("preferences");
+        birds = new Texture[3];
 
-		topTube = new Texture("can.png");
-		bottomTube = new Texture("can.png");
+        birds[0] = new Texture("playerup.png"); //face is up
+        birds[1] = new Texture("playerfall.png"); // face is down
 
-		maxTubeOffset = Gdx.graphics.getHeight() / 2 - gap / 2 - 100;
-		randomGenerator = new Random();
-		distanceBetweenTubes = Gdx.graphics.getWidth() * 3 / 4;
-		topTubeRectangles = new Rectangle[numberOfTubes];
-		bottomTubeRectangles = new Rectangle[numberOfTubes];
+        topTube = new Texture("can.png");
+        bottomTube = new Texture("can.png");
 
-
-		GlyphLayout layout = new GlyphLayout(endGameMessage, END_GAME_MESSAGE);
-		endgameTextWidth = layout.width;
-
-		layout = new GlyphLayout(endGameMessage, START_GAME_MESSAGE);
-		startGameTextWidth = layout.width;
-
-		layout = new GlyphLayout(highScoreMessage, HIGH_SCORE_MESSAGE + preferences.getInteger(PREFERENCES_HIGHSCORE));
-		highScoreTextWidth = layout.width;
-
-		startGame();
-		startMusic();
-	}
-
-	public void startGame() {
-		birdY = Gdx.graphics.getHeight() / 2 - birds[0].getHeight() / 2;
-		endtime = 0;
-		for (int i = 0; i < numberOfTubes; i++) {
-			tubeOffset[i] = (randomGenerator.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - gap - 200);
-			tubeX[i] = Gdx.graphics.getWidth() / 2 - topTube.getWidth() / 2 + Gdx.graphics.getWidth() + i * distanceBetweenTubes;
-			topTubeRectangles[i] = new Rectangle();
-			bottomTubeRectangles[i] = new Rectangle();
-		}
-	}
-
-	@Override
-	public void render() {
-		batch.begin();
-		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
-		if (gameState == 1) {
-			batch.draw(birds[flapState], Gdx.graphics.getWidth() / 2 - birds[flapState].getWidth() / 2, birdY);
-			if (tubeX[scoringTube] < Gdx.graphics.getWidth() / 2) {
-				score++;
-				if (scoringTube < numberOfTubes - 1) {
-					scoringTube++;
-				} else {
-					scoringTube = 0;
-				}
-			}
-			if (Gdx.input.justTouched()) {
-				velocity = -20;
-				flapState = 0;
-				recordedHeight = birdY;
-			}else{
-				if(recordedHeight > (birdY +30) ){
-					flapState = 1;
-				}
-			}
-
-			for (int i = 0; i < numberOfTubes; i++) {
-				if (tubeX[i] < - topTube.getWidth()) {
-					tubeX[i] += numberOfTubes * distanceBetweenTubes;
-					tubeOffset[i] = (randomGenerator.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - gap - 200);
-				} else {
-					tubeX[i] = tubeX[i] - tubeVelocity;
-				}
-				batch.draw(topTube, tubeX[i], Gdx.graphics.getHeight() / 2 + gap / 2 + tubeOffset[i]);
-				batch.draw(bottomTube, tubeX[i], Gdx.graphics.getHeight() / 2 - gap / 2 - bottomTube.getHeight() + tubeOffset[i]);
-				topTubeRectangles[i] = new Rectangle(tubeX[i], Gdx.graphics.getHeight() / 2 + gap / 2 + tubeOffset[i], topTube.getWidth(), topTube.getHeight());
-				bottomTubeRectangles[i] = new Rectangle(tubeX[i], Gdx.graphics.getHeight() / 2 - gap / 2 - bottomTube.getHeight() + tubeOffset[i], bottomTube.getWidth(), bottomTube.getHeight());
-			}
-
-			if (birdY > 0) {
-				velocity = velocity + gravity;
-				birdY -= velocity;
-			} else {
-				gameState = 2;
-			}
-
-		} else if (gameState == 0) {
-			if (Gdx.input.justTouched()) {
-				gameState = 1;
-				startGameMessage.dispose();
-			}else{
-				startGameMessage.draw(batch, START_GAME_MESSAGE,Gdx.graphics.getWidth() /2 - startGameTextWidth /2,
-						Gdx.graphics.getHeight()/2);
-			}
-
-		} else if (gameState == 2) {
-			gameOver();
-		}
-
-		scoreMessage.draw(batch, String.valueOf(score), 100, 200);
-
-		birdCircle.set(Gdx.graphics.getWidth() / 2, birdY + birds[flapState].getHeight() / 2, birds[flapState].getWidth() / 2);
-
-		for (int i = 0; i < numberOfTubes; i++) {
-			if (Intersector.overlaps(birdCircle, topTubeRectangles[i]) || Intersector.overlaps(birdCircle, bottomTubeRectangles[i])) {
-				gameState = 2;
-			}
-		}
-
-		batch.end();
-
-	}
-
-	public void saveHighScore(int currentScore){
-		int maxScore = preferences.getInteger(PREFERENCES_HIGHSCORE, 0);
-		if(currentScore > maxScore){
-			preferences.putInteger(PREFERENCES_HIGHSCORE, currentScore);
-			preferences.flush();
-		}
-	}
+        maxTubeOffset = Gdx.graphics.getHeight() / 2 - gap / 2 - 100;
+        randomGenerator = new Random();
+        distanceBetweenTubes = Gdx.graphics.getWidth() * 3 / 4;
+        topTubeRectangles = new Rectangle[numberOfTubes];
+        bottomTubeRectangles = new Rectangle[numberOfTubes];
 
 
-	public void startMusic(){
-		sound = Gdx.audio.newMusic(Gdx.files.internal("song.mp3"));
-		sound.play();
-		sound.setLooping(true);
-	}
+        GlyphLayout layout = new GlyphLayout(endGameMessage, END_GAME_MESSAGE);
+        endgameTextWidth = layout.width;
 
-	public void gameOver(){
-		batch.draw(gameOverBackground, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		saveHighScore(score);
+        layout = new GlyphLayout(endGameMessage, START_GAME_MESSAGE);
+        startGameTextWidth = layout.width;
 
-		endGameMessage.draw(batch, "R E S T A R T",Gdx.graphics.getWidth() /2 - endgameTextWidth /2,
-				Gdx.graphics.getHeight()/2);
+        layout = new GlyphLayout(highScoreMessage, HIGH_SCORE_MESSAGE + preferences.getInteger(PREFERENCES_HIGHSCORE));
+        highScoreTextWidth = layout.width;
 
-		highScoreMessage.draw(batch, HIGH_SCORE_MESSAGE + preferences.getInteger(PREFERENCES_HIGHSCORE),Gdx.graphics.getWidth() /2 - highScoreTextWidth /2,
-				Gdx.graphics.getHeight()/2 + 300);
+        startGame();
+        startMusic();
+    }
 
-		endtime = endtime == 0 ? TimeUtils.millis() : endtime;
+    public void startGame() {
+        birdY = Gdx.graphics.getHeight() / 2 - birds[0].getHeight() / 2;
+        endtime = 0;
+        for (int i = 0; i < numberOfTubes; i++) {
+            tubeOffset[i] = (randomGenerator.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - gap - 200);
+            tubeX[i] = Gdx.graphics.getWidth() / 2 - topTube.getWidth() / 2 + Gdx.graphics.getWidth() + i * distanceBetweenTubes;
+            topTubeRectangles[i] = new Rectangle();
+            bottomTubeRectangles[i] = new Rectangle();
+        }
+    }
 
-		if(TimeUtils.timeSinceMillis(endtime)>333)
-			if (Gdx.input.justTouched()) {
-				gameState = 1;
-				startGame();
-				score = 0;
-				scoringTube = 0;
-				velocity = 0;
-			}
-	}
+    @Override
+    public void render() {
+        batch.begin();
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        if (gameState == 1) {
+            batch.draw(birds[flapState], Gdx.graphics.getWidth() / 2 - birds[flapState].getWidth() / 2, birdY);
+            if (tubeX[scoringTube] < Gdx.graphics.getWidth() / 2) {
+                score++;
+                if (scoringTube < numberOfTubes - 1) {
+                    scoringTube++;
+                } else {
+                    scoringTube = 0;
+                }
+            }
+            if (Gdx.input.justTouched()) {
+                velocity = -20;
+                flapState = 0;
+                recordedHeight = birdY;
+            } else {
+                if (recordedHeight > (birdY + 30)) {
+                    flapState = 1;
+                }
+            }
+
+            for (int i = 0; i < numberOfTubes; i++) {
+                if (tubeX[i] < -topTube.getWidth()) {
+                    tubeX[i] += numberOfTubes * distanceBetweenTubes;
+                    tubeOffset[i] = (randomGenerator.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - gap - 200);
+                } else {
+                    tubeX[i] = tubeX[i] - tubeVelocity;
+                }
+                batch.draw(topTube, tubeX[i], Gdx.graphics.getHeight() / 2 + gap / 2 + tubeOffset[i]);
+                batch.draw(bottomTube, tubeX[i], Gdx.graphics.getHeight() / 2 - gap / 2 - bottomTube.getHeight() + tubeOffset[i]);
+                topTubeRectangles[i] = new Rectangle(tubeX[i], Gdx.graphics.getHeight() / 2 + gap / 2 + tubeOffset[i], topTube.getWidth(), topTube.getHeight());
+                bottomTubeRectangles[i] = new Rectangle(tubeX[i], Gdx.graphics.getHeight() / 2 - gap / 2 - bottomTube.getHeight() + tubeOffset[i], bottomTube.getWidth(), bottomTube.getHeight());
+            }
+
+            if (birdY > 0) {
+                velocity = velocity + gravity;
+                birdY -= velocity;
+            } else {
+                gameState = 2;
+            }
+
+        } else if (gameState == 0) {
+            if (Gdx.input.justTouched()) {
+                gameState = 1;
+                startGameMessage.dispose();
+            } else {
+                startGameMessage.draw(batch, START_GAME_MESSAGE, Gdx.graphics.getWidth() / 2 - startGameTextWidth / 2,
+                        Gdx.graphics.getHeight() / 2);
+            }
+
+        } else if (gameState == 2) {
+            gameOver();
+        }
+
+        scoreMessage.draw(batch, String.valueOf(score), 100, 200);
+
+        birdCircle.set(Gdx.graphics.getWidth() / 2, birdY + birds[flapState].getHeight() / 2, birds[flapState].getWidth() / 2);
+
+        for (int i = 0; i < numberOfTubes; i++) {
+            if (Intersector.overlaps(birdCircle, topTubeRectangles[i]) || Intersector.overlaps(birdCircle, bottomTubeRectangles[i])) {
+                gameState = 2;
+            }
+        }
+
+        batch.end();
+
+    }
+
+    public void saveHighScore(int currentScore) {
+        int maxScore = preferences.getInteger(PREFERENCES_HIGHSCORE, 0);
+        if (currentScore > maxScore) {
+            preferences.putInteger(PREFERENCES_HIGHSCORE, currentScore);
+            preferences.flush();
+        }
+    }
+
+
+    public void startMusic() {
+        sound = Gdx.audio.newMusic(Gdx.files.internal("song.mp3"));
+        sound.play();
+        sound.setLooping(true);
+    }
+
+    public void gameOver() {
+        batch.draw(gameOverBackground, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        saveHighScore(score);
+
+        endGameMessage.draw(batch, "R E S T A R T", Gdx.graphics.getWidth() / 2 - endgameTextWidth / 2,
+                Gdx.graphics.getHeight() / 2);
+
+        highScoreMessage.draw(batch, HIGH_SCORE_MESSAGE + preferences.getInteger(PREFERENCES_HIGHSCORE), Gdx.graphics.getWidth() / 2 - highScoreTextWidth / 2,
+                Gdx.graphics.getHeight() / 2 + 300);
+
+        endtime = endtime == 0 ? TimeUtils.millis() : endtime;
+
+        if (TimeUtils.timeSinceMillis(endtime) > 333)
+            if (Gdx.input.justTouched()) {
+                gameState = 1;
+                startGame();
+                score = 0;
+                scoringTube = 0;
+                velocity = 0;
+            }
+    }
 
 }
